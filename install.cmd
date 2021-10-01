7468026eea00b096e2c878e4b2fa364db8f28a5b
kapellan2000 <a.v.slobodyanyuk@gmail.com>
2021-10-01 19:53:51 +0300

    fix 10.1.2021 v3

--- install.cmd
+++ install.cmd
@@ -4,13 +4,14 @@ echo -----------------
 
 set /p Input=Input path to Prism root: 
 cd %~dp0
-dir
-pause
 rmdir %Input%"\Plugins\Apps\Cinema"
 mkdir %Input%"\Plugins\Apps\Cinema"
+del %Input%"\ProjectFiles\EmptyScenes\EmptyScene Cinema R23.c4d"
 
 xcopy ".\Integration" %Input%"\Plugins\Apps\Cinema\Integration" /s /e /y /i /o
 xcopy ".\Scripts" %Input%"\Plugins\Apps\Cinema\Scripts" /s /e /y /i /o
-xcopy ".\Integration\EmptyScene Cinema R23.c4d" %Input%"\ProjectFiles\EmptyScenes" /s /e /y /i /o
+xcopy /S /Q /Y /F ".\Integration\EmptyScene Cinema R23.c4d" %Input%"\ProjectFiles\EmptyScenes\"
+
+
 echo Complite
 pause
