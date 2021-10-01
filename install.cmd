2d9da854f53674a14fd865bcd23a159744395263
kapellan2000 <a.v.slobodyanyuk@gmail.com>
2021-10-01 19:41:17 +0300

    fix 10.1.2021_2

--- install.cmd
+++ install.cmd
@@ -1,15 +1,16 @@
 @echo off
 echo Cinema plugin add
 echo -----------------
+
 set /p Input=Input path to Prism root: 
-DirName=%cd% 
-cd %DirName%
+cd %~dp0
 dir
+pause
 rmdir %Input%"\Plugins\Apps\Cinema"
 mkdir %Input%"\Plugins\Apps\Cinema"
 
-xcopy %DirName%"\Integration" %Input%"\Plugins\Apps\Cinema\Integration" /s /e /y /i /o
-xcopy %DirName%"\Scripts" %Input%"\Plugins\Apps\Cinema\Scripts" /s /e /y /i /o
-
+xcopy ".\Integration" %Input%"\Plugins\Apps\Cinema\Integration" /s /e /y /i /o
+xcopy ".\Scripts" %Input%"\Plugins\Apps\Cinema\Scripts" /s /e /y /i /o
+xcopy ".\Integration\EmptyScene Cinema R23.c4d" %Input%"\ProjectFiles\EmptyScenes" /s /e /y /i /o
 echo Complite
 pause
