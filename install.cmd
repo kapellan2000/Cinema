662757de3f373bea2c0a08590a650fe5669b5ae5
kapellan2000 <a.v.slobodyanyuk@gmail.com>
2021-11-20 13:14:22 +0200

    instlla fix

--- install.cmd
+++ install.cmd
@@ -10,7 +10,7 @@ del %Input%"\ProjectFiles\EmptyScenes\EmptyScene Cinema R23.c4d"
 
 xcopy ".\Integration" %Input%"\Plugins\Apps\Cinema\Integration" /s /e /y /i /o
 xcopy ".\Scripts" %Input%"\Plugins\Apps\Cinema\Scripts" /s /e /y /i /o
-xcopy ".\Scripts" %Input%"\Plugins\Apps\Cinema\UserInterfaces" /s /e /y /i /o
+xcopy ".\UserInterfaces" %Input%"\Plugins\Apps\Cinema\UserInterfaces" /s /e /y /i /o
 
 xcopy /S /Q /Y /F ".\Integration\EmptyScene Cinema R23.c4d" %Input%"\ProjectFiles\EmptyScenes\"
 
